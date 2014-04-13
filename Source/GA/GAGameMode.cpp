cbc5b1680e44013e71aa0893ebd26e2b10e3a1a1
Jack_Harb@gmx.de <Jack_Harb@gmx.de@435454b9-d0bb-46eb-8854-52d2cd3ebfb7>
2014-04-13 16:06:01 +0000

    ~ fixed MyCharacter loading bug

--- Source/GA/GAGameMode.cpp
+++ Source/GA/GAGameMode.cpp
@@ -7,7 +7,7 @@ AGAGameMode::AGAGameMode(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FObjectFinder<UClass> PlayerPawnBPClass(TEXT("Class'/Game/Blueprints/MyCharacter.MyCharacter_C'"));
+	static ConstructorHelpers::FObjectFinder<UClass> PlayerPawnBPClass(TEXT("Class'/Game/Blueprints/PlayerCharacter.PlayerCharacter_C'"));
 	if (PlayerPawnBPClass.Object != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Object;
