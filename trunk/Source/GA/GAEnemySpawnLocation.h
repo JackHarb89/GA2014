8c3a1d984a43edd9014bce0c0c93ea57b8b9d8e0
Jack_Harb@gmx.de <Jack_Harb@gmx.de@435454b9-d0bb-46eb-8854-52d2cd3ebfb7>
2014-04-21 14:01:29 +0000

    + added missing spawns. commit fail...

---
+++ trunk/Source/GA/GAEnemySpawnLocation.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "GAEnemySpawnLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGAEnemySpawnLocation : public AActor
+{
+	GENERATED_UCLASS_BODY()
+
+};
