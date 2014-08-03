54eb161c9adb148159d9096d8074bbf74e8c3556
Jack_Harb@gmx.de <Jack_Harb@gmx.de@435454b9-d0bb-46eb-8854-52d2cd3ebfb7>
2014-08-03 13:04:10 +0000

    + added HUD for MainMenu / Transitionmap

--- trunk/Source/GA/GAPlayerController.cpp
+++ trunk/Source/GA/GAPlayerController.cpp
@@ -10,6 +10,10 @@
 AGAPlayerController::AGAPlayerController(const class FPostConstructInitializeProperties& PCIP)
 : Super(PCIP)
 {
+	static ConstructorHelpers::FObjectFinder<UBlueprint> HUD_Menu(TEXT("/Game/UI/Classes/GA_MainMenu.GA_MainMenu"));
+	static ConstructorHelpers::FObjectFinder<UBlueprint> HUD_Transition(TEXT("/Game/UI/Classes/GA_MainMenu.GA_MainMenu"));
+	MainMenuHud = (UClass*)HUD_Menu.Object->GeneratedClass;
+	TransitionHud = (UClass*)HUD_Menu.Object->GeneratedClass;
 }
 
 void AGAPlayerController::PlayerTick(float DeltaTime) {
@@ -26,13 +30,12 @@ void AGAPlayerController::PlayerTick(float DeltaTime) {
 // Connecting To Given Server IP. IP Example: "127.0.0.1:7777"
 void AGAPlayerController::ConnectToServer(const FString& ip){
 	UE_LOG(LogClass, Log, TEXT("*** TRYING TO CONNECT TO SERVER ***"));
-	ClientTravel(ip, TRAVEL_Absolute, true);
+	ClientTravel(ip, TRAVEL_Absolute, false);
 }
 
 void AGAPlayerController::ChangeMap(const FString& mapName){
 	UE_LOG(LogClass, Log, TEXT("*** CHANGING MAP ***"));
 	FString UrlString = TEXT("/Game/Maps/" + mapName);
-
 	GetWorld()->ServerTravel(UrlString);
 }
 
@@ -47,11 +50,34 @@ void AGAPlayerController::HostGameWithPort(int32 Port){
 
 void AGAPlayerController::GetSeamlessTravelActorList(bool bToEntry, TArray<AActor*>& ActorList){
 	Super::GetSeamlessTravelActorList(bToEntry, ActorList);
-	for (int32 i = 0 ; i < ((AGA_HUD*)MyHUD)->currentSpawnedAreas.Num() ; i++){
+	for (int32 i = 0; i < ((AGA_HUD*)MyHUD)->currentSpawnedAreas.Num(); i++){
 		ActorList.Add(((AGA_HUD*)MyHUD)->currentSpawnedAreas[i]);
 	}
 }
 
 void AGAPlayerController::PreClientTravel(const FString& PendingURL, ETravelType TravelType, bool bIsSeamlessTravel){
 	Super::PreClientTravel(PendingURL, TravelType, bIsSeamlessTravel);
+}
+
+void AGAPlayerController::ClientSetHUD_Implementation(TSubclassOf<AHUD> NewHUDClass){
+	if (MyHUD != NULL)
+	{
+		MyHUD->Destroy();
+		MyHUD = NULL;
+	}
+	FActorSpawnParameters SpawnInfo;
+	SpawnInfo.Owner = this;
+	SpawnInfo.Instigator = Instigator;
+	MyHUD = GetWorld()->SpawnActor<AHUD>(NewHUDClass, SpawnInfo);
+
+	if (GetLevel()->OwningWorld->GetName().Contains("SG_Game")){
+		MyHUD = GetWorld()->SpawnActor<AHUD>(NewHUDClass, SpawnInfo);
+	}
+	else if (GetLevel()->OwningWorld->GetName().Contains("SG_TransitionMap")){
+		MyHUD = GetWorld()->SpawnActor<AHUD>(TransitionHud, SpawnInfo);
+	}
+	else if (GetLevel()->OwningWorld->GetName().Contains("SG_MainMenu")){
+		MyHUD = GetWorld()->SpawnActor<AHUD>(MainMenuHud, SpawnInfo);
+	}
+
 }
